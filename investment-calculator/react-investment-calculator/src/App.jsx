52b8e08182306adad845362ea18c5da59d8a77a3
msr <msr@opentext.com>
2025-12-29 08:15:11 +0530

    Initial investment Calculator Project Setup with HTML Template

--- investment-calculator/react-investment-calculator/src/App.jsx
+++ investment-calculator/react-investment-calculator/src/App.jsx
@@ -1,33 +1,82 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <section className="user-input">
+          <div className="input-group">
+            <p>
+              <label>Initial Investment</label>
+              <input type="number" />
+            </p>
+            <p>
+              <label>Annual Investment</label>
+              <input type="number" />
+            </p>
+          </div>
+          <div className="input-group">
+            <p>
+              <label>Expected Return</label>
+              <input type="number" />
+            </p>
+            <p>
+              <label>Duration in Years</label>
+              <input type="number" />
+            </p>
+          </div>
+        </section>
+        <table className="returns">
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Investment Value</th>
+              <th>Interest (Year)</th>
+              <th>Total Interest</th>
+              <th>Invested Capital</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>$2,060</td>
+              <td>$60</td>
+              <td>$60</td>
+              <td>$2,000</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>$3,184</td>
+              <td>$124</td>
+              <td>$184</td>
+              <td>$3,000</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>$4,375</td>
+              <td>$191</td>
+              <td>$375</td>
+              <td>$4,000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>$5,637</td>
+              <td>$262</td>
+              <td>$637</td>
+              <td>$5,000</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>$6,975</td>
+              <td>$338</td>
+              <td>$975</td>
+              <td>$6000</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
     </>
   )
 }
